659855344f5a469883474513fbfa1ce5aa023eef
Andriikryl <andreyk557@gmail.com>
2023-04-17 15:12:40 +0300

    feat[handleSubmit-function]

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+
+    setNewItem("");
+  }
+
   return (
     <div className="App">
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">new item</label>
-          <input type="text" id="item" />
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
         </div>
         <button className="btn btn-add">Add</button>
       </form>
       <h1 className="title">Todo List</h1>
       <ul>
-        <li>
-          <label>
-            <input type="checkbox" />
-            item 1
-          </label>
-          <button className="btn btn-delet">Delete</button>
-        </li>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input type="checkbox" checked={todo.completed} />
+                {todo.title}
+              </label>
+              <button className="btn btn-delet">Delete</button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
