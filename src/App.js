80004f90aa1d3e1ac2fa86cfef8dc657b7de50da
Andriikryl <andreyk557@gmail.com>
2023-04-17 14:52:04 +0300

    feat[form-component]

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">new item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn btn-add">Add</button>
+      </form>
+      <h1 className="title">Todo List</h1>
+      <ul>
+        <li>
+          <label>
+            <input type="checkbox" />
+            item 1
+          </label>
+          <button className="btn btn-delet">Delete</button>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
